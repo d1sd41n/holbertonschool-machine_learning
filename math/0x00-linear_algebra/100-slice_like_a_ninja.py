01c1441eb2f9c6c93bcc7dbc0882addb06145365
Daniel Perez <1498@holbertonschool.com>
2020-12-02 12:49:06 -0500

    15

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -2,3 +2,16 @@
 """
 here some unnecessary documentation :)
 """
+
+
+def np_slice(matrix, axes={}):
+    """
+    slices the matrix
+    """
+    aux = []
+    for row in range(0, len(matrix.shape)):
+        if axes.get(row, None):
+            aux.append(slice(*axes.get(row, None)))
+            continue
+        aux.append(slice(None))
+    return matrix[tuple(aux)]
