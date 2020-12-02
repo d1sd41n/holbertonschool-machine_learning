32ffe86b08891d912f03b00cf26ce7513bb95e73
Daniel Perez <1498@holbertonschool.com>
2020-12-02 11:35:38 -0500

    baking up

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""
+here some unnecessary documentation :)
+"""
---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+here some unnecessary documentation :)
+"""
+
+
+def np_transpose(matrix):
+    """
+    transposes array
+    """
+    return matrix.T
