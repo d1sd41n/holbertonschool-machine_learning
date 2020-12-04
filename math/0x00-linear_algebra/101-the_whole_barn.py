ecfaeb173fa5249d3cef79745c9056f41ee19680
Daniel Perez <1498@holbertonschool.com>
2020-12-04 12:38:23 -0500

    16

--- math/0x00-linear_algebra/101-the_whole_barn.py
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -2,3 +2,13 @@
 """
 here some unnecessary documentation :)
 """
+import numpy as np
+
+
+def add_matrices(mat1, mat2):
+    """
+    Add two matrices
+    """
+    if np.shape(mat1) != np.shape(mat1):
+        return None
+    return np.add(mat1, mat2)
