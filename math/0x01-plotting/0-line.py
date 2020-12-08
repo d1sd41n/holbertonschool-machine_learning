aea7171e51998e9f9a42a97b7cfdb12e016cac9c
Daniel Alejandro <outnumbered@live.com>
2020-12-07 19:48:36 -0500

    2

---
+++ math/0x01-plotting/0-line.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+here some unnecessary documentation
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+y = np.arange(0, 11) ** 3
+
+plt.plot(y, 'r')
+plt.show()
