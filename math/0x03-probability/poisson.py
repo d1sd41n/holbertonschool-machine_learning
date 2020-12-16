aed83d1bd833252e7a76eb803fd53bc01e226a5c
Daniel Pérez <ddd666.dt@gmail.com>
2020-12-15 21:45:51 -0500

    2#

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -12,7 +12,7 @@ class Poisson:
         if data is not None:
             if len(data) <= 2:
                 raise ValueError("data must contain multiple values")
-            elif type(data) != list:
+            elif type(data) is not list:
                 raise TypeError("data must be a list")
             else:
                 self.lambtha = float(sum(data) / len(data))
@@ -21,3 +21,34 @@ class Poisson:
                 raise ValueError("lambtha must be a positive value")
             else:
                 self.lambtha = float(lambtha)
+
+    def pmf(self, k):
+        """docs"""
+        if k < 0:
+            return 0
+        euler = 2.7182818285
+        aux_factorio = 1
+        aux_k = k + 1
+        for i in range(1, aux_k):
+            aux_factorio = aux_factorio * i
+        res = ((euler **
+                (-1 * self.lambtha)) *
+               (self.lambtha ** k)) * (1 / aux_factorio)
+        return res
+
+    def cdf(self, k):
+        """docs"""
+        if k < 0:
+            return 0
+        euler = 2.7182818285
+        aux_factorio = 1
+        aux_k = k + 1
+        aux_s = 0
+        for i in range(0, aux_k):
+            aux_factorio = 1
+            n_aux = i + 1
+            for j in range(1, n_aux):
+                aux_factorio = aux_factorio * j
+            aux_s = aux_s + (self.lambtha ** i) * (1 / aux_factorio)
+        res = (euler ** (-1 * self.lambtha)) * aux_s
+        return res
