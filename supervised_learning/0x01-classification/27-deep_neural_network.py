b55354323411fb888d4e4e478c96e49d1fd55468
Daniel Pérez <1498@holbertonschool.com>
2021-01-03 17:08:57 -0500

    27

--- supervised_learning/0x01-classification/27-deep_neural_network.py
+++ supervised_learning/0x01-classification/27-deep_neural_network.py
@@ -1,80 +1,301 @@
 #!/usr/bin/env python3
+"""[summary]
 
-import matplotlib.pyplot as plt
+Raises:
+    TypeError: [description]
+    ValueError: [description]
+    TypeError: [description]
+    TypeError: [description]
+"""
 import numpy as np
+import matplotlib.pyplot as plt
+import pickle
 
-Deep3 = __import__('27-deep_neural_network').DeepNeuralNetwork
-Deep4 = __import__('28-deep_neural_network').DeepNeuralNetwork
-one_hot_encode = __import__('24-one_hot_encode').one_hot_encode
-one_hot_decode = __import__('25-one_hot_decode').one_hot_decode
 
-lib = np.load('../data/MNIST.npz')
-X_train_3D = lib['X_train']
-Y_train = lib['Y_train']
-X_valid_3D = lib['X_valid']
-Y_valid = lib['Y_valid']
-X_test_3D = lib['X_test']
-Y_test = lib['Y_test']
-X_train = X_train_3D.reshape((X_train_3D.shape[0], -1)).T
-X_valid = X_valid_3D.reshape((X_valid_3D.shape[0], -1)).T
-X_test = X_test_3D.reshape((X_test_3D.shape[0], -1)).T
-Y_train_one_hot = one_hot_encode(Y_train, 10)
-Y_valid_one_hot = one_hot_encode(Y_valid, 10)
-Y_test_one_hot = one_hot_encode(Y_test, 10)
+class DeepNeuralNetwork:
+    """[summary]
+    """
 
-print('Sigmoid activation:')
-deep3 = Deep3.load('3-output.pkl')
-A_one_hot3, cost3 = deep3.evaluate(X_train, Y_train_one_hot)
-A3 = one_hot_decode(A_one_hot3)
-accuracy3 = np.sum(Y_train == A3) / Y_train.shape[0] * 100
-print("Train cost:", cost3)
-print("Train accuracy: {}%".format(accuracy3))
-A_one_hot3, cost3 = deep3.evaluate(X_valid, Y_valid_one_hot)
-A3 = one_hot_decode(A_one_hot3)
-accuracy3 = np.sum(Y_valid == A3) / Y_valid.shape[0] * 100
-print("Validation cost:", cost3)
-print("Validation accuracy: {}%".format(accuracy3))
-A_one_hot3, cost3 = deep3.evaluate(X_test, Y_test_one_hot)
-A3 = one_hot_decode(A_one_hot3)
-accuracy3 = np.sum(Y_test == A3) / Y_test.shape[0] * 100
-print("Test cost:", cost3)
-print("Test accuracy: {}%".format(accuracy3))
+    def __init__(self, nx, layers):
+        """[summary]
 
-fig = plt.figure(figsize=(10, 10))
-for i in range(100):
-    fig.add_subplot(10, 10, i + 1)
-    plt.imshow(X_test_3D[i])
-    plt.title(A3[i])
-    plt.axis('off')
-plt.tight_layout()
-plt.show()
+        Args:
+            nx features
+            layers array with layers
 
-print('\nTanh activaiton:')
+        Raises:
+            TypeError: [description]
+            ValueError: [description]
+            TypeError: [description]
+            TypeError: [description]
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list):
+            raise TypeError("layers must be a list of positive integers")
+        if len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
 
-deep4 = Deep4.load('4-saved.pkl')
-A_one_hot4, cost4 = deep4.train(X_train, Y_train_one_hot, iterations=100,
-                                step=10, graph=False)
-A4 = one_hot_decode(A_one_hot4)
-accuracy4 = np.sum(Y_train == A4) / Y_train.shape[0] * 100
-print("Train cost:", cost4)
-print("Train accuracy: {}%".format(accuracy4))
-A_one_hot4, cost4 = deep4.evaluate(X_valid, Y_valid_one_hot)
-A4 = one_hot_decode(A_one_hot4)
-accuracy4 = np.sum(Y_valid == A4) / Y_valid.shape[0] * 100
-print("Validation cost:", cost4)
-print("Validation accuracy: {}%".format(accuracy4))
-A_one_hot4, cost4 = deep4.evaluate(X_test, Y_test_one_hot)
-A4 = one_hot_decode(A_one_hot4)
-accuracy4 = np.sum(Y_test == A4) / Y_test.shape[0] * 100
-print("Test cost:", cost4)
-print("Test accuracy: {}%".format(accuracy4))
-deep4.save('4-output')
+        self.__nx = nx
+        self.__layers = layers
+        self.__cache = {}
+        self.__weights = {}
+        self.__L = len(layers)
 
-fig = plt.figure(figsize=(10, 10))
-for i in range(100):
-    fig.add_subplot(10, 10, i + 1)
-    plt.imshow(X_test_3D[i])
-    plt.title(A4[i])
-    plt.axis('off')
-plt.tight_layout()
-plt.show()
+        for i in range(0, len(layers)):
+            if not isinstance(layers[i], int) or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+
+            w_lk = "W{}".format(i + 1)
+            b_k = "b{}".format(i + 1)
+
+            self.__weights[b_k] = np.zeros((layers[i], 1))
+
+            if i == 0:
+                self.__weights[w_lk] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.__weights[w_lk] = np.random.randn(
+                    layers[i], layers[i - 1]) * np.sqrt(2 / layers[i - 1])
+
+    @property
+    def L(self):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
+        return self.__L
+
+    @property
+    def cache(self):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """
+        return self.__weights
+
+    def sigmoid(self, Z):
+        """[summary]
+
+        Args:
+            Z ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        return 1 / (1 + np.exp(-Z))
+
+    def softmax(self, x):
+        """[summary]
+
+        Args:
+            x ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum(axis=0)
+
+    def forward_prop(self, X):
+        """[summary]
+
+        Args:
+            X ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        self.__cache['A0'] = X
+        for i in range(0, self.L):
+            W = self.weights['W{}'.format(i + 1)]
+            b = self.weights['b{}'.format(i + 1)]
+            A = self.cache['A' + str(i)]
+            Z = np.matmul(W, A) + b
+            A2 = "A{}".format(i + 1)
+            if i == self.L - 1:
+                self.__cache[A2] = self.softmax(Z)
+            else:
+                self.__cache[A2] = self.sigmoid(Z)
+        return (self.__cache[A2], self.cache)
+
+    def cost(self, Y, A):
+        """[summary]
+
+        Args:
+            Y ([type]): [description]
+            A ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        return -1 * np.sum(Y * np.log(A)) / Y.shape[1]
+
+    def evaluate(self, X, Y):
+        """[summary]
+
+        Args:
+            X ([type]): [description]
+            Y ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        A = self.forward_prop(X)[0]
+        Y_hat = np.max(A, axis=0)
+        return np.where(A == Y_hat, 1, 0), self.cost(Y, A)
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+        """[summary]
+
+        Args:
+            Y ([type]): [description]
+            cache ([type]): [description]
+            alpha (float, optional): [description]. Defaults to 0.05.
+        """
+        m = Y.shape[1]
+        weights_c = self.weights.copy()
+        for i in reversed(range(1, self.__L + 1)):
+            w_k = "W{}".format(i)
+            b_k = "b{}".format(i)
+            A = cache['A{}'.format(i)]
+            if i == self.L:
+                dz = A - Y
+            else:
+                dz = A * (1 - A) * np.matmul(
+                    weights_c["W{}".format(i + 1)].T, dz)
+            self.weights[w_k] = self.weights[w_k] - alpha * \
+                (np.matmul(dz, cache["A{}".format(i - 1)].T) / m)
+            self.weights[b_k] = self.weights[b_k] - alpha * \
+                (np.sum(dz, axis=1, keepdims=True) / m)
+
+    def train(self, X, Y, iterations=500, alpha=0.05,
+              verbose=True, graph=True, step=100):
+        """[summary]
+
+        Args:
+            X ([type]): [description]
+            Y ([type]): [description]
+            iterations (int, optional): [description]. Defaults to 500.
+            alpha (float, optional): [description]. Defaults to 0.05.
+            verbose (bool, optional): [description]. Defaults to True.
+            graph (bool, optional): [description]. Defaults to True.
+            step (int, optional): [description]. Defaults to 100.
+
+        Raises:
+            TypeError: [description]
+            ValueError: [description]
+            TypeError: [description]
+            ValueError: [description]
+            TypeError: [description]
+            ValueError: [description]
+
+        Returns:
+            [type]: [description]
+        """
+
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+
+        if iterations <= 0:
+            raise ValueError("iterations must be a positive integer")
+
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
+
+        if verbose is True or graph is True:
+            if not isinstance(step, int):
+                raise TypeError("step must be an integer")
+            if step <= 0 or step > iterations:
+                raise ValueError("step must be positive and <= iterations")
+
+        # cost = []
+        # iters = []
+
+        # for i in range(iterations):
+        #     self.forward_prop(X)
+        #     self.gradient_descent(X, Y, self.__A1, self.__A2, alpha)
+
+        #     if i % step == 0 or i == iterations:
+        #         cost.append(self.cost(Y, self.__A2))
+        #         iters.append(i)
+
+        #         if verbose:
+        #             print("Cost after {} iterations: {}".
+        #                   format(i, self.cost(Y, self.__A2)))
+
+        # if graph:
+        #     plt.plot(iters, cost, '-b')
+        #     plt.xlabel('iteration')
+        #     plt.ylabel('cost')
+
+        #     plt.title('Training Cost')
+        #     plt.show()
+
+        # return self.evaluate(X, Y)
+
+        cost = []
+        iters = []
+        for i in range(iterations):
+            A, self.__cache = self.forward_prop(X)
+            self.gradient_descent(Y, self.__cache, alpha)
+
+            if i % step == 0 or step == iterations:
+                cost.append(self.cost(Y, A))
+                iters.append(i)
+
+                if verbose:
+                    print("Cost after {} iterations: {}".format(
+                        i, self.cost(Y, A)))
+
+        if graph:
+            plt.plot(iters, cost, '-b')
+            plt.xlabel('iteration')
+            plt.ylabel('cost')
+            plt.title("Trainig Cost")
+            plt.show()
+
+        return self.evaluate(X, Y)
+
+    def save(self, filename):
+        """[summary]
+
+        Args:
+            filename ([type]): [description]
+        """
+        if not filename.endswith(".pkl"):
+            filename = filename + ".pkl"
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    @staticmethod
+    def load(filename):
+        """[summary]
+
+        Args:
+            filename ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except FileNotFoundError:
+            return None
