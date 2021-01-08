045a5377b6f8abf3015e689f954d1c666c0c14d9
Daniel Pérez <1498@holbertonschool.com>
2021-01-07 22:12:30 -0500

    7

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -8,9 +8,8 @@ create_train_op = __import__('5-create_train_op').create_train_op
 forward_prop = __import__('2-forward_prop').forward_prop
 
 
-def train(X_train, Y_train, X_valid, Y_valid,
-          layer_sizes, activations, alpha, iterations,
-          save_path="/tmp/model.ckpt"):
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha,
+          iterations, save_path="/tmp/model.ckpt"):
     """[summary]
 
     Args:
@@ -27,27 +26,39 @@ def train(X_train, Y_train, X_valid, Y_valid,
     Returns:
         [type]: [description]
     """
-
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
     y_pred = forward_prop(x, layer_sizes, activations)
+    tf.add_to_collection('y_pred', y_pred)
     accuracy = calculate_accuracy(y, y_pred)
+    tf.add_to_collection('accuracy', accuracy)
     loss = calculate_loss(y, y_pred)
+    tf.add_to_collection('loss', loss)
     train_op = create_train_op(loss, alpha)
+    tf.add_to_collection('train_op', train_op)
+
     init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+
     with tf.Session() as sess:
         sess.run(init)
-        saver = tf.train.Saver()
-        for i in range(iterations):
-            _, accur_train, loss_value_train = sess.run(
-                (train_op, accuracy, loss), feed_dict={x: X_train, y: Y_train})
-            _, accur_val, loss_value_val = sess.run(
-                (train_op, accuracy, loss), feed_dict={x: X_valid, y: Y_valid})
+        for i in range(iterations + 1):
+            cost_train, accuracy_train = sess.run(
+                [loss, accuracy],
+                feed_dict={x: X_train, y: Y_train})
+            cost_valid, accuracy_valid = sess.run(
+                [loss, accuracy],
+                feed_dict={x: X_valid, y: Y_valid})
 
-            if i % 100 == 0:
+            if i % 100 == 0 or i == iterations:
                 print("After {} iterations:".format(i))
-                print("\tTraining Cost: {}".format(loss_value_train))
-                print("\tTraining Accuracy: {}".format(accur_train))
-                print("\tValidation Cost: {}".format(loss_value_val))
-                print("\tValidation Accuracy: {}".format(accur_val))
-        saver.save(sess, save_path)
+                print("\tTraining Cost: {}".format(cost_train))
+                print("\tTraining Accuracy: {}".format(accuracy_train))
+                print("\tValidation Cost: {}".format(cost_valid))
+                print("\tValidation Accuracy: {}".format(accuracy_valid))
+            if i < iterations:
+                sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+        save_path = saver.save(sess, save_path)
+
     return save_path
