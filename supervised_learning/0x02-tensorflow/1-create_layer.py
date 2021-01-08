3d4a5224d0712c7f998f191c9aec89b0872a6d5b
Daniel Pérez <1498@holbertonschool.com>
2021-01-07 19:48:25 -0500

    1

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -7,7 +7,8 @@ def create_layer(prev, n, activation):
     """tensorflow 1"""
     he_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     layer = tf.layers.Dense(
-        units=n, kernel_initializer=he_init, activation=activation, name='layer')
+        units=n, kernel_initializer=he_init,
+        activation=activation, name='layer')
     l_output = layer(prev)
 
     return l_output
