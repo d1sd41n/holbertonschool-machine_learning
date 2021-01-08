644dc2f5ca156e686f8cf8f3df1a81c66c4b613b
Daniel Pérez <1498@holbertonschool.com>
2021-01-07 22:19:49 -0500

    6

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""[summary]"""
+"""[summary]
+
+Returns:
+    [type]: [description]
+"""
 import tensorflow as tf
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
