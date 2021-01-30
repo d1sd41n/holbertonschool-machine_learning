4d1357986962445cd4c20adfd26f3f92afb5e662
Daniel Pérez <1498@holbertonschool.com>
2021-01-29 22:37:10 -0500

    all

--- supervised_learning/0x06-keras/11-config.py
+++ supervised_learning/0x06-keras/11-config.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python3
+"""[summary]
 
-import numpy as np
-one_hot = __import__('3-one_hot').one_hot
-load_model = __import__('9-model').load_model
-test_model = __import__('12-test').test_model
+Returns:
+    [type]: [description]
+"""
+import tensorflow.keras as K
 
 
-if __name__ == '__main__':
-    datasets = np.load('../data/MNIST.npz')
-    X_test = datasets['X_test']
-    X_test = X_test.reshape(X_test.shape[0], -1)
-    Y_test = datasets['Y_test']
-    Y_test_oh = one_hot(Y_test)
+def save_config(network, filename):
+    """[summary]
 
-    network = load_model('network2.h5')
-    print(test_model(network, X_test, Y_test_oh))
+    Args:
+        network ([type]): [description]
+        filename ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    with open(filename, 'w') as fil:
+        fil.write(network.to_json())
+    return None
+
+
+def load_config(filename):
+    """[summary]
+
+    Args:
+        filename ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    with open(filename, "r") as fil:
+        return K.models.model_from_json(fil.read())
