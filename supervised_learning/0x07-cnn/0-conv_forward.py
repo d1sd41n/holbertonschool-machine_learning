8cdf888ae2780313dfbd32c541b36a504397a629
Daniel Pérez <1498@holbertonschool.com>
2021-02-03 01:44:22 -0500

    0

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -30,17 +30,17 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
         ph = int(((h_prev - 1) * sh - h_prev + kh) / 2)
         pw = int(((w_prev - 1) * sw - w_prev + kw) / 2)
 
-    padded = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                    mode='constant',
-                    constant_values=0)
+    pad = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                 mode='constant',
+                 constant_values=0)
     z_h = int((h_prev + 2 * ph - kh) / sh + 1)
     z_w = int((w_prev + 2 * pw - kw) / sw + 1)
     z = np.zeros((m, z_h, z_w, c_new))
     for i in range(z_h):
         for j in range(z_w):
             for k in range(c_new):
-                z[:, i, j, k] = (padded[:, i * sh: i * sh + kh,
-                                        j * sw: j * sw + kw, :] *
+                z[:, i, j, k] = (pad[:, i * sh: i * sh + kh,
+                                     j * sw: j * sw + kw, :] *
                                  W[:, :, :, k]).sum(axis=(1, 2, 3))
                 z[:, i, j, k] = activation(z[:, i, j, k] + b[0, 0, 0, k])
     return z
