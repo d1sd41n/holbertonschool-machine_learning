0e689a4e1251788d41c629475f0587e805d0049e
Daniel Pérez <1498@holbertonschool.com>
2021-02-11 22:14:48 -0500

    fuck pep8

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -5,6 +5,7 @@ Returns:
     [type]: [description]
 """
 import tensorflow.keras as K
+
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
 
@@ -49,4 +50,5 @@ def densenet121(growth_rate=32, compression=1.0):
                        activation='softmax',
                        kernel_initializer=he_ini
                        )(l)
-    return K.models.Model(inputs=input_, outputs=l)
+    m = K.models.Model(inputs=input_, outputs=l)
+    return m
