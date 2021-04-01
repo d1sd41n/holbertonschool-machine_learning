0d0a601441f90d4ed858ab8c0f006f69a772d4d2
Daniel Perez <1498@holbertonschool.com>
2021-04-01 00:23:28 -0500

    1

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -5,83 +5,44 @@ Returns:
     [type]: [description]
 """
 import numpy as np
+pdf = __import__('5-pdf').pdf
 
 
-def distances(a, b):
-    """[summary]
-
-    Args:
-        a ([type]): [description]
-        b ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    b = b[np.newaxis, :]
-    a = a[:, np.newaxis, :]
-    diff = a - b
-    dist = np.linalg.norm(diff, axis=-1, keepdims=False)
-    return dist
-
-
-def initialize(X, k):
+def expectation(X, pi, m, S):
     """[summary]
 
     Args:
         X ([type]): [description]
-        k ([type]): [description]
+        pi ([type]): [description]
+        m ([type]): [description]
+        S ([type]): [description]
 
     Returns:
         [type]: [description]
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None
-    if not isinstance(k, int) or k <= 0 or k >= X.shape[0]:
-        return None
-    mins = np.min(X, axis=0)
-    maxs = np.max(X, axis=0)
-    centroids = np.random.uniform(mins, maxs, size=(k, X.shape[1]))
-    return centroids
-
-
-def kmeans(X, k, iterations=1000):
-    """[summary]
-
-    Args:
-        X ([type]): [description]
-        k ([type]): [description]
-        iterations (int, optional): [description]. Defaults to 1000.
-
-    Returns:
-        [type]: [description]
-    """
-    # (k, d)
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None
-    if type(iterations) is not int or iterations < 1:
+    if type(m) is not np.ndarray or len(m.shape) != 2:
         return None, None
-    if not isinstance(k, int) or k <= 0 or k >= X.shape[0]:
+    if type(S) is not np.ndarray or len(S.shape) != 3:
         return None, None
-    C = initialize(X, k)
-    if C is None:
+    if type(pi) is not np.ndarray or len(pi.shape) != 1:
         return None, None
-    k, d = C.shape
-    n, _ = X.shape
-    dist = np.zeros(n)
-    for i in range(iterations):
-        dists = distances(X, C)
-        clss = np.argmin(dists, axis=1).reshape(-1)
-        C_changed = False
-        for j in range(k):
-            clust = X[clss == j]
-            if len(clust) == 0:
-                mean_j = initialize(X, 1)[0]
-            else:
-                mean_j = clust.mean(axis=0)
-
-            if (mean_j != C[j]).all():
-                C[j] = mean_j
-                C_changed = True
-        if not C_changed:
-            break
-    return C, clss
+    if X.shape[1] != S.shape[1] or S.shape[1] != S.shape[2]:
+        return (None, None)
+    if X.shape[1] != m.shape[1] or m.shape[0] != S.shape[0]:
+        return (None, None)
+    if pi.shape[0] != m.shape[0]:
+        return (None, None)
+    if not np.isclose(np.sum(pi), 1):
+        return None, None
+    n, d = X.shape
+    k = S.shape[0]
+    t = np.zeros((k, n))
+    for i in range(k):
+        P = pdf(X, m[i], S[i])
+        prior = pi[i]
+        t[i] = prior * P
+    g = t / np.sum(t, axis=0)
+    likelihood = np.sum(np.log(np.sum(t, axis=0)))
+    return g, likelihood
