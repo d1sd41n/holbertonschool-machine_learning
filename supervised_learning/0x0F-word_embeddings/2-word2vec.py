5485d8850687ca5a5013665d5040a369be0aa8b1
Daniel Perez <1498@holbertonschool.com>
2021-05-11 22:41:02 -0500

    1

---
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""[summary]
+
+Returns:
+    [type]: [description]
+"""
+import tensorflow.keras as keras
+from gensim.models import Word2Vec
+
+
+def word2vec_model(sentences, size=100, min_count=5,
+                   window=5, negative=5,
+                   cbow=True, iterations=5,
+                   seed=0, workers=1):
+    """[summary]
+
+    Args:
+        sentences ([type]): [description]
+        size (int, optional): [description]. Defaults to 100.
+        min_count (int, optional): [description]. Defaults to 5.
+        window (int, optional): [description]. Defaults to 5.
+        negative (int, optional): [description]. Defaults to 5.
+        cbow (bool, optional): [description]. Defaults to True.
+        iterations (int, optional): [description]. Defaults to 5.
+        seed (int, optional): [description]. Defaults to 0.
+        workers (int, optional): [description]. Defaults to 1.
+
+    Returns:
+        [type]: [description]
+    """
+    w2v = Word2Vec(sentences, size=size,
+                   window=window,
+                   min_count=min_count,
+                   negative=negative,
+                   workers=workers,
+                   sg=cbow, seed=seed,
+                   iter=iterations)
+    return w2v.train(sentences,
+                     total_examples=w2v.corpus_count,
+                     epochs=w2v.epochs)
