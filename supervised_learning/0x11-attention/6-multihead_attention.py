39d9a1a654bfbdeb541c46035580b3bd261ae2a0
Daniel Perez <1498@holbertonschool.com>
2021-05-21 22:01:30 -0500

    7

---
+++ supervised_learning/0x11-attention/6-multihead_attention.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+"""[summary]
+
+Returns:
+    [type]: [description]
+"""
+import tensorflow as tf
+sdp_attention = __import__('5-sdp_attention').sdp_attention
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+    """[summary]
+
+    Args:
+        tf ([type]): [description]
+    """
+
+    def __init__(self, dm, h):
+        """[summary]
+
+        Args:
+            dm ([type]): [description]
+            h ([type]): [description]
+        """
+
+        super(MultiHeadAttention, self).__init__()
+        self.h = h
+        self.dm = dm
+        self.depth = int(
+            self.dm // self.h)
+        self.Wq = tf.keras.layers.Dense(dm)
+        self.Wk = tf.keras.layers.Dense(dm)
+        self.Wv = tf.keras.layers.Dense(dm)
+        self.linear = tf.keras.layers.Dense(dm)
+
+    def splitHeads(self, m, batch):
+        """[summary]
+
+        Args:
+            m ([type]): [description]
+            batch ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+        m = tf.reshape(m, (batch, -1, self.h, self.depth))
+        return tf.transpose(m, perm=[0, 2, 1, 3])
+
+    def call(self, Q, K, V, mask):
+        """[summary]
+
+        Args:
+            Q ([type]): [description]
+            K ([type]): [description]
+            V ([type]): [description]
+            mask ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
+
+        batch = tf.shape(K)[0]
+        Q = self.Wq(Q)
+        K = self.Wk(K)
+        V = self.Wv(V)
+        Q = self.splitHeads(Q,
+                            batch)
+        K = self.splitHeads(K, batch)
+        V = self.splitHeads(V, batch)
+        output, w = sdp_attention(
+            Q, K, V, mask)
+        output = tf.transpose(output,
+                              perm=[0, 2, 1, 3])
+        output = tf.reshape(output,
+                            (batch, -1, self.dm))
+        return self.linear(
+            output), w
