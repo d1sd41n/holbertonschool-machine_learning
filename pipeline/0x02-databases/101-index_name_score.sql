df4d83fe7c0e9300ddb46dfbc83a4efa034debc9
Daniel Pérez <1498@holbertonschool.com>
2021-08-02 22:57:45 -0500

    0

---
+++ pipeline/0x02-databases/101-index_name_score.sql
@@ -0,0 +1,2 @@
+-- doc
+CREATE INDEX idx_name_first_score ON names (name(1), score);
---
+++ pipeline/0x02-databases/102-need_meeting.sql
@@ -0,0 +1,6 @@
+-- doc
+CREATE VIEW need_meeting AS
+SELECT name 
+FROM students
+WHERE score < 80 AND
+(last_meeting IS NULL OR DATEDIFF(CURDATE(),last_meeting) > 30);
