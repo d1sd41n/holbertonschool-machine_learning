b5f8f8cd8dc3570398f5e1f5c805e1e0e670bdd8
Daniel Pérez <1498@holbertonschool.com>
2021-08-02 23:22:23 -0500

    0

--- pipeline/0x02-databases/106-log_stats.py
+++ pipeline/0x02-databases/106-log_stats.py
@@ -5,7 +5,8 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient(
+        'mongodb://127.0.0.1:27017')
     logs_collection = client.logs.nginx
     
     
@@ -23,23 +24,38 @@ if __name__ == "__main__":
     
     print("Methods:")
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    methods = ["GET",
+               "POST",
+               "PUT",
+               "PATCH",
+               "DELETE"]
+    
     for method in methods:
-        num_method = logs_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, num_method))
+        num_method = logs_collection.count_documents(
+            {"method": method})
+        print("\tmethod {}: {}".format(
+            method, num_method))
 
 
 
 
-    filter_path = {"method": "GET", "path": "/status"}
-    num_path = logs_collection.count_documents(filter_path)
+    filter_path = {
+        "method": "GET",
+        "path": "/status"}
+    num_path = logs_collection.count_documents(
+        filter_path)
     
     
     
     
     print("{} status check".format(num_path))
     print("IPs:")
-    pipeline = [{"$group": {"_id": "$ip", "count": {"$sum": 1}}}]
+    pipeline = [
+        {"$group": {"_id": "$ip",
+                    "count": {"$sum": 1}
+                    }
+         }
+        ]
     
     
     
@@ -49,7 +65,8 @@ if __name__ == "__main__":
     list_ips = []
     for ip in ips:
         list_ips.append(ip)
-    sorted_ips = sorted(list_ips, key=lambda i: i["count"], reverse=True)
+    sorted_ips = sorted(
+        list_ips, key=lambda i: i["count"], reverse=True)
 
     i = 0
     limi______________________________________________________________________________________t = 10
@@ -61,7 +78,9 @@ if __name__ == "__main__":
         limi______________________________________________________________________________________t = len(
             sorted_ips)
     while i < limi______________________________________________________________________________________t:
-        ip = sorted_ips[i]["_id"]
-        count = sorted_ips[i]["count"]
+        ip = sorted_ips[
+            i]["_id"]
+        count = sorted_ips[
+            i]["count"]
         print("\t{}: {}".format(ip, count))
         i += 1
