17d22ef3124b69cc587af6bb8a7cc27e4872e512
Daniel Pérez <1498@holbertonschool.com>
2021-08-02 23:09:07 -0500

    0

--- pipeline/0x02-databases/106-log_stats.py
+++ pipeline/0x02-databases/106-log_stats.py
@@ -5,21 +5,29 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """[summary]
-    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
     count_logs = nginx_collection.count_documents({})
     path = {'method': 'GET', 'path': '/status'}
     status = nginx_collection.count_documents(path)
-    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method = [
+        "GET",
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE"
+    ]
     print('{} logs'.format(count_logs))
     print('Methods:')
     for m in method:
         n_data = nginx_collection.count_documents({'method': m})
         print('\tmethod {}: {}'.format(m, n_data))
     print('{} status check'.format(status))
-    pipeline = [{'$unwind': '$ip'}, {"$sortByCount": '$ip'}, {'$limit': 10}]
+    pipeline = [
+        {'$unwind': '$ip'},
+        {"$sortByCount": '$ip'},
+        {'$limit': 10},
+    ]
     a_ip = nginx_collection.aggregate(pipeline)
     data_ip = [i for i in a_ip]
     print('IPs:')
