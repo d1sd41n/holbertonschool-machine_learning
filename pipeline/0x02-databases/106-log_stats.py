9ead489a1a8ddae139475ce2fd12fe0cc91c8856
Daniel Pérez <1498@holbertonschool.com>
2021-08-02 23:12:17 -0500

    0

--- pipeline/0x02-databases/106-log_stats.py
+++ pipeline/0x02-databases/106-log_stats.py
@@ -1,35 +1,33 @@
 #!/usr/bin/env python3
-"""[summary]
-"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-    count_logs = nginx_collection.count_documents({})
-    path = {'method': 'GET', 'path': '/status'}
-    status = nginx_collection.count_documents(path)
-    method = [
-        "GET",
-        "POST",
-        "PUT",
-        "PATCH",
-        "DELETE"
-    ]
-    print('{} logs'.format(count_logs))
-    print('Methods:')
-    for m in method:
-        n_data = nginx_collection.count_documents({'method': m})
-        print('\tmethod {}: {}'.format(m, n_data))
-    print('{} status check'.format(status))
-    pipeline = [
-        {'$unwind': '$ip'},
-        {"$sortByCount": '$ip'},
-        {'$limit': 10},
-    ]
-    a_ip = nginx_collection.aggregate(pipeline)
-    data_ip = [i for i in a_ip]
-    print('IPs:')
-    for ip in data_ip:
-        print('\t{}: {}'.format(ip['_id'], ip['count']))
+    logs_collection = client.logs.nginx
+    num_doc = logs_collection.count_documents({})
+    print("{} logs".format(num_doc))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        num_method = logs_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, num_method))
+    filter_path = {"method": "GET", "path": "/status"}
+    num_path = logs_collection.count_documents(filter_path)
+    print("{} status check".format(num_path))
+    print("IPs:")
+    pipeline = [{"$group": {"_id": "$ip", "count": {"$sum": 1}}}]
+    ips = logs_collection.aggregate(pipeline)
+    list_ips = []
+    for ip in ips:
+        list_ips.append(ip)
+    sorted_ips = sorted(list_ips, key=lambda i: i["count"], reverse=True)
+    i = 0
+    limit = 10
+    if len(sorted_ips) < 10:
+        limit = len(sorted_ips)
+    while i < limit:
+        ip = sorted_ips[i]["_id"]
+        count = sorted_ips[i]["count"]
+        print("\t{}: {}".format(ip, count))
+        i += 1
